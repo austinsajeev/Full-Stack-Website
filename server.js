8d894666503b98d16ea42b5b2c685f333947c698
austinsajeev <130976958+austinsajeev@users.noreply.github.com>
2026-02-10 10:23:22 +0530

    Add root route to serve web.html

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('.'));
 
+// Serve web.html on root
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'web.html'));
+});
+
 // --- MONGODB CONNECTION ---
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
